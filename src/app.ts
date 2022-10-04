99249930c1afc3c624d46fad54904e9fcb91b204
luigi tanaka <luigi.tanaka12@gmail.com>
2022-10-03 21:41:02 -0300

    feat: cria aplicação básica com express

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+
+const PORT = process.env.PORT || 5000;
+
+export default app;
