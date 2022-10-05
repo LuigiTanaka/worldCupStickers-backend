b987e54dd901966e415f5737dafd12144c9e86cf
luigi tanaka <luigi.tanaka12@gmail.com>
2022-10-05 15:39:23 -0300

    fix: vincula rotas de autenticação com a aplicação

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { json } from "express";
 import cors from "cors";
 import "express-async-errors";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
+import router from "./routes/index";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(json());
 
+app.use(router);
 app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 5000;
