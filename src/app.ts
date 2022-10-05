a3e259073c9d5e562094c82d3235c1d0be55ceb1
luigi tanaka <luigi.tanaka12@gmail.com>
2022-10-05 12:24:04 -0300

    feat: merge com a main

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import express, { json } from "express";
 import cors from "cors";
 import "express-async-errors";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
-import router from "./routes/index";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -12,7 +11,6 @@ const app = express();
 app.use(cors());
 app.use(json());
 
-app.use(router);
 app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 5000;
